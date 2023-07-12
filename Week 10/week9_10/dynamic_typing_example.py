ed14f8a7f65dc07fa7b3334da15a69ab44727388
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2023-07-12 19:58:32 +0300

    Week 10 folder name update

--- Week 10/week9_10/dynamic_typing_example.py
+++
@@ -1,9 +0,0 @@
-n= 10
-print (n)
-n = "dynamically typed"
-print (n)
-
-# print ('strongly typed' + 3)  # Incompatible operations between objects cause errors
-# print ('strongly typed' + ' and dynamically typed')
-
-
