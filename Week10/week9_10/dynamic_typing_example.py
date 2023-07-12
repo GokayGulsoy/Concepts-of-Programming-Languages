ed1556ac989d892e12912a75f3ce440810bc29be
GÖKAY GÜLSOY <111280608+GokayGulsoy@users.noreply.github.com>
2023-07-12 19:59:06 +0300

    Week 10 uploaded

---
+++ Week10/week9_10/dynamic_typing_example.py
@@ -0,0 +1,9 @@
+n= 10
+print (n)
+n = "dynamically typed"
+print (n)
+
+# print ('strongly typed' + 3)  # Incompatible operations between objects cause errors
+# print ('strongly typed' + ' and dynamically typed')
+
+
---
+++ Week10/week9_10/incompatible_type_example.java
@@ -0,0 +1,17 @@
+public class Main
+{
+	public static void main(String[] args) {
+	 
+	  int i = 68;
+	  System.out.println(i);
+      
+      char kar = 'd';
+      i = kar; 
+   
+   //   kar = i;  // incompatible types
+   //   kar = (char)i; // explicit type declaration is needed
+      System.out.println(i);
+
+	}
+}
+
